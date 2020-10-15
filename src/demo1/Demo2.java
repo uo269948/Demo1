9c1c00e129ff5bcb88fca01cae5b16c801d8ce61
uo269948 <uo269948@uniovi.es>
2020-10-15 16:39:37 +0200

    creacion demo2

---
+++ src/demo1/Demo2.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("linea 1");
+		System.out.print("linea 2");
+		System.out.println("linea 3");
+	}
+
+}
